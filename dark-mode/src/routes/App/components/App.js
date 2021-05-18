a154035fdec3facad18fce16dcc743c7d6c89bf0
ImInYourPie <iminyourpie@gmail.com>
2021-05-18 10:33:20 +0100

    Finished dark-mode

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import '../styles/_app.scss';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import "../styles/_app.scss";
+
+const DARK_MODE = "dark-mode";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const storageFlag = localStorage.getItem("darkMode");
+    if (storageFlag) return JSON.parse(storageFlag);
+    else return false;
+  });
+
+  const switchTheme = () => {
+    setIsDarkMode((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    const root = document.getElementById("root");
+    if (isDarkMode) root.className = DARK_MODE;
+    else root.removeAttribute("class");
+    localStorage.setItem("darkMode", isDarkMode);
+  }, [isDarkMode]);
+
   return (
     <div className="app">
       <div className="level">
@@ -12,18 +31,37 @@ function App() {
         </div>
 
         {/* --The button that should toggle dark mode-- */}
-        <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+        <button
+          onClick={switchTheme}
+          className="app__dark-mode-btn icon level-right"
+        >
+          {isDarkMode}
+          <FontAwesomeIcon
+            icon={isDarkMode ? faSun : faMoon}
+            color={isDarkMode ? "#FFA500" : "black"}
+          />
         </button>
-
       </div>
 
       <div className="columns">
         <div className="column">
-          <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
+          <p>
+            Lollipop powder powder. Cotton candy caramels chupa chups halvah
+            muffin caramels apple pie topping cake. Topping chocolate bar pastry
+            chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+            plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+            pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+            tiramisu chocolate bar carrot cake lemon drops halvah.
+          </p>
         </div>
         <div className="column">
-          <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
+          <p>
+            Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+            tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+            marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+            Pudding sugar plum chocolate cake cake biscuit pastry pastry
+            chocolate bar tart. Lemon drops dessert gummies icing.
+          </p>
         </div>
       </div>
 
